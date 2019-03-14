740d01eecdaad7edb29048508dbec4620353062e
Adrian Rollett <acrollet@gmail.com>
2019-03-14 15:36:54 -0600

    Add message classes and tests OD-57

---
+++ src/ResponseEngine/Message/Webchat/EmptyMessage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OpenDialogAi\ResponseEngine\Message\Webchat;
+
+/**
+ * Class EmptyMessage
+ *
+ * An empty message will cause the ResponseEngine to just send an HTTP 200 with no content.
+ *
+ * Please do not use as part of an array of messages.
+ *
+ * @package OpenDialogAi\ResponseEngine\Message\Webchat
+ */
+class EmptyMessage extends WebChatMessage
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setAsEmpty();
+    }
+}
