75b640cb8c87920dfa8811c27f4bc5e2eefc9c5a
Adrian Rollett <acrollet@gmail.com>
2019-04-02 15:34:10 -0600

    Add module skeleton OD-99

---
+++ src/SensorEngine/SensorEngineServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OpenDialogAi\SensorEngine;
+
+use Illuminate\Support\ServiceProvider;
+use OpenDialogAi\SensorEngine\Service\SensorEngineService;
+
+class SensorEngineServiceProvider extends ServiceProvider
+{
+    const SENSOR_ENGINE_SERVICE = 'sensor-engine-service';
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/opendialog-sensorengine.php'
+                => base_path('config/opendialog-sensorengine.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/opendialog-sensorengine.php', 'opendialog.sensor_engine');
+
+        $this->app->bind(self::SENSOR_ENGINE_SERVICE, function () {
+            return new SensorEngineService();
+        });
+    }
+}
