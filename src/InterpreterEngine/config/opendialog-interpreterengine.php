3671c10cbabfdc23ce4eb347aa077835db0a02cf
Stuart Haigh <stuarth@greenshootlabs.com>
2019-04-04 18:35:58 +0100

    bind luis interpreter OD-149

--- src/InterpreterEngine/config/opendialog-interpreterengine.php
+++ src/InterpreterEngine/config/opendialog-interpreterengine.php
@@ -23,6 +23,6 @@ return [
 
     // Register the application interfaces
     'available_interpreters' => [
-        // \OpenDialogAi\InterpreterEngine\tests\Interpreters\DummyInterpreter::class,
+         \InterpreterEngine\Interpreters\LuisInterpreter::class
     ]
 ];
