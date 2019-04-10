e2aa01a585296fcb1a8aae28b85e6cbbf11796c5
Ronald Ashri <ronald@istos.it>
2019-04-10 23:37:34 +0200

    Update interface.

--- src/ConversationEngine/ConversationStore/ConversationStoreInterface.php
+++ src/ConversationEngine/ConversationStore/ConversationStoreInterface.php
@@ -4,8 +4,11 @@
 namespace OpenDialogAi\ConversationEngine\ConversationStore;
 
 use Ds\Map;
+use OpenDialogAi\Core\Conversation\Conversation;
 
 interface ConversationStoreInterface
 {
     public function getAllOpeningIntents(): Map;
+
+    public function getConversation($conversationId): Conversation;
 }
