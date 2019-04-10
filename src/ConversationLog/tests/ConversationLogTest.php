6e61d4b9eaee581e7e516a60f3b1c2da8d3427c8
Adrian Rollett <acrollet@gmail.com>
2019-04-10 10:39:23 -0600

    Add package skeleton
    OD-177

---
+++ src/ConversationLog/tests/ConversationLogTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OpenDialogAi\Core\Tests\Unit;
+
+use OpenDialogAi\Core\Graph\DGraph\DGraphClient;
+use OpenDialogAi\Core\Graph\DGraph\DGraphQuery;
+use OpenDialogAi\Core\Tests\TestCase;
+use Spatie\Activitylog\Models\Activity;
+
+class ConversationLogTest extends TestCase
+{
+}
