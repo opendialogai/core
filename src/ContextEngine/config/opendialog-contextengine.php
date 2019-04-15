a5be73b716a99cfd7bdc335f3d4e9cb5fba73dd4
Ronald Ashri <ronald@istos.it>
2019-04-15 17:15:45 +0200

    Some additional core attributes.

--- src/ContextEngine/config/opendialog-contextengine.php
+++ src/ContextEngine/config/opendialog-contextengine.php
@@ -3,6 +3,11 @@
 return [
     'supported_attributes' => [
         'name' => \OpenDialogAi\Core\Attribute\StringAttribute::class,
+        'first_name' => \OpenDialogAi\Core\Attribute\StringAttribute::class,
+        'last_name' => \OpenDialogAi\Core\Attribute\StringAttribute::class,
+        'full_name' => \OpenDialogAi\Core\Attribute\StringAttribute::class,
+        'external_id' => \OpenDialogAi\Core\Attribute\StringAttribute::class,
+        'email' => \OpenDialogAi\Core\Attribute\StringAttribute::class,
         'id' => \OpenDialogAi\Core\Attribute\StringAttribute::class,
         'ei_type' => \OpenDialogAi\Core\Attribute\StringAttribute::class,
         'timestamp' => \OpenDialogAi\Core\Attribute\IntAttribute::class,
