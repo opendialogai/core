106c8f2fe35a0437f177bae6ec8278aaf6927a55
Ronald Ashri <ronald@istos.it>
2019-04-19 17:14:13 +0200

    Add operations needed for test.

--- src/Attribute/IntAttribute.php
+++ src/Attribute/IntAttribute.php
@@ -45,6 +45,10 @@ class IntAttribute extends AbstractAttribute
                 break;
             case AbstractAttribute::GREATER_THAN_OR_EQUAL:
                 return $this->testGreaterThanOrEqual($attribute);
+                break;
+            case AbstractAttribute::GREATER_THAN:
+                return $this->testGreaterThan($attribute);
+                break;
             default:
                 return false;
         }
@@ -74,6 +78,20 @@ class IntAttribute extends AbstractAttribute
 
         return false;
     }
+
+    /**
+     * @param AttributeInterface $attribute
+     * @return bool
+     */
+    private function testGreaterThan(AttributeInterface $attribute)
+    {
+        if ($this->getValue() > $attribute->getValue()) {
+            return true;
+        }
+
+        return false;
+    }
+
 
     /**
      * @return string
