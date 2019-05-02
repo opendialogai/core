bc2d323faa33403becda6b7111feb84fc9f1bbfa
Adrian Rollett <acrollet@gmail.com>
2019-05-02 09:15:16 -0600

    Add conversation export command

---
+++ src/Console/Commands/ExportConversation.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace OpenDialogAi\Core\Console\Commands;
+
+use Illuminate\Console\Command;
+use OpenDialogAi\ConversationBuilder\Conversation;
+use OpenDialogAi\Core\Conversation\Conversation as CoreConversation;
+use OpenDialogAi\ResponseEngine\MessageTemplate;
+use OpenDialogAi\ResponseEngine\OutgoingIntent;
+
+class ExportConversation extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'export:conversation {conversation id} {--f|filename=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Export a conversation and its intents + outgoing messages';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Load the conversation.
+        $conversationId = $this->argument('conversation');
+        $conversation = Conversation::findOrFail($conversationId);
+
+        // Find this conversation's intents.
+        $outgoingIntents = [];
+        $messageTemplates = [];
+        $parsedConversation = $conversation->buildConversation();
+        foreach ($parsedConversation->getAllIntents() as $intent) {
+            if ($outgoingIntent = OutgoingIntent::where('name', $intent->getLabel())->with('messageTemplates')->first()) {
+                if (!isset($outgoingIntents[$outgoingIntent->id])) {
+                    $outgoingIntents[$outgoingIntent->id] = $outgoingIntent;
+                    foreach ($outgoingIntent->messageTemplates as $messageTemplate) {
+                        if (!isset($messageTemplates[$messageTemplate->id])) {
+                            $messageTemplates[$messageTemplate->id] = $messageTemplate;
+                        }
+                    }
+                }
+            }
+        }
+
+        $output = serialize([
+            'conversation' => $conversation,
+            'outgoingIntents' => $outgoingIntents,
+            'messageTemplates' => $messageTemplates,
+        ]);
+
+        $filename = $this->option('filename');
+
+        if ($filename) {
+            file_put_contents($filename, $output);
+        } else {
+            print($output);
+        }
+    }
+}
--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -5,6 +5,7 @@ namespace OpenDialogAi\Core;
 use Illuminate\Support\ServiceProvider;
 use OpenDialogAi\ContextEngine\ContextManager\ContextService;
 use OpenDialogAi\ConversationEngine\ConversationEngineInterface;
+use OpenDialogAi\Core\Console\Commands\ExportConversation;
 use OpenDialogAi\Core\Controllers\OpenDialogController;
 use OpenDialogAi\Core\Graph\DGraph\DGraphClient;
 use OpenDialogAi\ResponseEngine\Service\ResponseEngineServiceInterface;
@@ -24,6 +25,12 @@ class CoreServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ExportConversation::class,
+            ]);
+        }
     }
 
     public function register()
