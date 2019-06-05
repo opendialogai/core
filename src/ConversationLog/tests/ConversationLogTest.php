18074a5d8b83bb3db04700aa6fec10fc66f711a0
Stuart Haigh <stuarth@greenshootlabs.com>
2019-06-05 16:43:28 +0100

    turns on all tests

--- src/ConversationLog/tests/ConversationLogTest.php
+++ src/ConversationLog/tests/ConversationLogTest.php
@@ -21,16 +21,16 @@ class ConversationLogTest extends TestCase
     public function testChatbotUserCanBeCreated()
     {
         ChatbotUser::create([
-          'user_id' => 'test@example.com',
-          'first_name' => 'Joe',
-          'last_name' => 'Cool',
-          'ip_address' => '127.0.0.1',
-          'country' => 'UK',
-          'browser_language' => 'en',
-          'os' => 'Mac OS X',
-          'browser' => 'Safari',
-          'timezone' => 'GMT',
-          'platform' => 'webchat',
+            'user_id' => 'test@example.com',
+            'first_name' => 'Joe',
+            'last_name' => 'Cool',
+            'ip_address' => '127.0.0.1',
+            'country' => 'UK',
+            'browser_language' => 'en',
+            'os' => 'Mac OS X',
+            'browser' => 'Safari',
+            'timezone' => 'GMT',
+            'platform' => 'webchat',
         ]);
         $chatbotUser = ChatbotUser::where('user_id', 'test@example.com')->first();
         $this->assertEquals('webchat', $chatbotUser->platform);
@@ -42,15 +42,15 @@ class ConversationLogTest extends TestCase
     public function testChatbotUserDbRelationships()
     {
         ChatbotUser::create([
-          'user_id' => 'test@example.com',
-          'first_name' => 'Joe',
-          'last_name' => 'Cool',
-          'ip_address' => '127.0.0.1',
-          'country' => 'UK',
-          'browser_language' => 'en',
-          'os' => 'Mac OS X',
-          'browser' => 'Safari',
-          'timezone' => 'GMT',
+            'user_id' => 'test@example.com',
+            'first_name' => 'Joe',
+            'last_name' => 'Cool',
+            'ip_address' => '127.0.0.1',
+            'country' => 'UK',
+            'browser_language' => 'en',
+            'os' => 'Mac OS X',
+            'browser' => 'Safari',
+            'timezone' => 'GMT',
         ]);
         $chatbotUser = ChatbotUser::where('user_id', 'test@example.com')->first();
 
@@ -70,22 +70,22 @@ class ConversationLogTest extends TestCase
     public function testWebchatHistoryEndpoint()
     {
         ChatbotUser::create([
-          'user_id' => 'test@example.com',
-          'first_name' => 'Joe',
-          'last_name' => 'Cool',
-          'ip_address' => '127.0.0.1',
-          'country' => 'UK',
-          'browser_language' => 'en',
-          'os' => 'Mac OS X',
-          'browser' => 'Safari',
-          'timezone' => 'GMT',
+            'user_id' => 'test@example.com',
+            'first_name' => 'Joe',
+            'last_name' => 'Cool',
+            'ip_address' => '127.0.0.1',
+            'country' => 'UK',
+            'browser_language' => 'en',
+            'os' => 'Mac OS X',
+            'browser' => 'Safari',
+            'timezone' => 'GMT',
         ]);
         $chatbotUser = ChatbotUser::where('user_id', 'test@example.com')->first();
 
-        $message = Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'test message')->save();
-        $message2 = Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'another test message')->save();
+        Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'test message')->save();
+        Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'another test message')->save();
 
-        $response = $this->get('/user/test@example.com/history?limit=5')
+        $this->get('/user/test@example.com/history?limit=5')
             ->assertStatus(200)
             ->assertJsonCount(2)
             ->assertJson([
@@ -99,28 +99,28 @@ class ConversationLogTest extends TestCase
     public function testWebchatHistoryEndpointIgnoreParam()
     {
         ChatbotUser::create([
-          'user_id' => 'test@example.com',
-          'first_name' => 'Joe',
-          'last_name' => 'Cool',
-          'ip_address' => '127.0.0.1',
-          'country' => 'UK',
-          'browser_language' => 'en',
-          'os' => 'Mac OS X',
-          'browser' => 'Safari',
-          'timezone' => 'GMT',
+            'user_id' => 'test@example.com',
+            'first_name' => 'Joe',
+            'last_name' => 'Cool',
+            'ip_address' => '127.0.0.1',
+            'country' => 'UK',
+            'browser_language' => 'en',
+            'os' => 'Mac OS X',
+            'browser' => 'Safari',
+            'timezone' => 'GMT',
         ]);
         $chatbotUser = ChatbotUser::where('user_id', 'test@example.com')->first();
 
-        $message = Message::create(microtime(), 'chat_open', $chatbotUser->user_id, 'me', '')->save();
-        $message2 = Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'test message')->save();
-        $message3 = Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'another test message')->save();
-        $message4 = Message::create(microtime(), 'trigger', $chatbotUser->user_id, 'me', '')->save();
+        Message::create(microtime(), 'chat_open', $chatbotUser->user_id, 'me', '')->save();
+        Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'test message')->save();
+        Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'another test message')->save();
+        Message::create(microtime(), 'trigger', $chatbotUser->user_id, 'me', '')->save();
 
-        $response = $this->get('/user/test@example.com/history?limit=10&ignore=chat_open,trigger')
+        $this->get('/user/test@example.com/history?limit=10&ignore=chat_open,trigger')
             ->assertStatus(200)
             ->assertJsonCount(2);
 
-        $response = $this->get('/user/test@example.com/history?limit=10')
+        $this->get('/user/test@example.com/history?limit=10')
             ->assertStatus(200)
             ->assertJsonCount(4);
     }
@@ -131,22 +131,22 @@ class ConversationLogTest extends TestCase
     public function testWebchatHistoryEndpointLimit()
     {
         ChatbotUser::create([
-          'user_id' => 'test@example.com',
-          'first_name' => 'Joe',
-          'last_name' => 'Cool',
-          'ip_address' => '127.0.0.1',
-          'country' => 'UK',
-          'browser_language' => 'en',
-          'os' => 'Mac OS X',
-          'browser' => 'Safari',
-          'timezone' => 'GMT',
+            'user_id' => 'test@example.com',
+            'first_name' => 'Joe',
+            'last_name' => 'Cool',
+            'ip_address' => '127.0.0.1',
+            'country' => 'UK',
+            'browser_language' => 'en',
+            'os' => 'Mac OS X',
+            'browser' => 'Safari',
+            'timezone' => 'GMT',
         ]);
         $chatbotUser = ChatbotUser::where('user_id', 'test@example.com')->first();
 
-        $message = Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'test message')->save();
-        $message2 = Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'another test message')->save();
+        Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'test message')->save();
+        Message::create(microtime(), 'text', $chatbotUser->user_id, 'me', 'another test message')->save();
 
-        $response = $this->get('/user/test@example.com/history?limit=1')
+        $this->get('/user/test@example.com/history?limit=1')
             ->assertStatus(200)
             ->assertJsonCount(1)
             ->assertJson([
@@ -185,19 +185,19 @@ class ConversationLogTest extends TestCase
             ->assertJson([0 => ['data' => ['text' => 'No messages found for intent intent.core.NoMatchResponse']]]);
 
         // Ensure that incoming messages are logged.
-				$this->assertDatabaseHas('messages', [
-						'user_id' => 'someuser',
-						'message' => 'test message',
-				]);
+        $this->assertDatabaseHas('messages', [
+            'user_id' => 'someuser',
+            'message' => 'test message',
+        ]);
 
         // Ensure that outgoing messages are logged.
-				$this->assertDatabaseHas('messages', [
-						'author' => 'them',
-						'message' => 'No messages found for intent intent.core.NoMatchResponse',
-				]);
+        $this->assertDatabaseHas('messages', [
+            'author' => 'them',
+            'message' => 'No messages found for intent intent.core.NoMatchResponse',
+        ]);
 
         // Ensure that the correct history is returned
-        $response = $this->get('/user/someuser/history?limit=2')
+        $this->get('/user/someuser/history?limit=2')
             ->assertStatus(200)
             ->assertJsonCount(2)
             ->assertJson([
@@ -207,11 +207,6 @@ class ConversationLogTest extends TestCase
 
     public function testInternalProperty()
     {
-        if (!getenv('LOCAL')) {
-            // This test depends on dGraph.
-            $this->markTestSkipped('This test only runs on local environments.');
-        }
-
         $validCallback = ['welcome' => 'intent.core.welcome'];
         $this->setConfigValue('opendialog.interpreter_engine.supported_callbacks', $validCallback);
 
