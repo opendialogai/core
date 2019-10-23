a226bfbb1cf7cc36f4235f5dcdb4e30372328830
elliotmassen <elliotm@greenshootlabs.com>
2019-10-23 17:02:10 +0100

    Adding tests for intent conditions

--- tests/Feature/NextIntentDetectionTest.php
+++ tests/Feature/NextIntentDetectionTest.php
@@ -83,6 +83,56 @@ class NextIntentDetectionTest extends TestCase
         $this->assertEquals($conversationContext->getAttributeValue('next_intent'), 'doing_dandy');
     }
 
+    public function testConditionsOnOutgoingIntents()
+    {
+        $this->setSupportedCallbacks([
+            'hello_bot' => 'hello_bot'
+        ]);
+
+        $openDialogController = resolve(OpenDialogController::class);
+
+        $this->activateConversation($this->testConversationWithOutgoingIntentConditions());
+
+        $conversationContext = ContextService::getConversationContext();
+
+        // Expect to get to 'response_without_name' because the name isn't set
+        $utterance = UtteranceGenerator::generateChatOpenUtterance('hello_bot');
+        $openDialogController->runConversation($utterance);
+        $this->assertEquals($conversationContext->getAttributeValue('interpreted_intent'), 'hello_bot');
+        $this->assertEquals($conversationContext->getAttributeValue('current_conversation'), 'with_outgoing_intent_conditions');
+        $this->assertEquals($conversationContext->getAttributeValue('current_scene'), 'opening_scene');
+        $this->assertEquals($conversationContext->getAttributeValue('next_intent'), 'response_without_name');
+    }
+
+    public function testConditionsIncomingIntents()
+    {
+        $this->setSupportedCallbacks([
+            'ask_to_send_me_email' => 'ask_to_send_me_email',
+            'send_email' => 'send_email'
+        ]);
+
+        $openDialogController = resolve(OpenDialogController::class);
+
+        $this->activateConversation($this->testConversationWithIncomingIntentConditions());
+
+        $conversationContext = ContextService::getConversationContext();
+
+        // Expect to get to 'ask_to_send_me_email' because the email isn't set
+        $utterance = UtteranceGenerator::generateChatOpenUtterance('ask_to_send_me_email');
+        $openDialogController->runConversation($utterance);
+        $this->assertEquals($conversationContext->getAttributeValue('interpreted_intent'), 'ask_to_send_me_email');
+        $this->assertEquals($conversationContext->getAttributeValue('current_conversation'), 'with_incoming_intent_conditions');
+        $this->assertEquals($conversationContext->getAttributeValue('current_scene'), 'opening_scene');
+        $this->assertEquals($conversationContext->getAttributeValue('next_intent'), 'ask_for_email');
+
+        // Expect to get to end of conversation
+        $openDialogController->runConversation(UtteranceGenerator::generateChatOpenUtterance('send_email', $utterance->getUser()));
+        $this->assertEquals($conversationContext->getAttributeValue('interpreted_intent'), 'send_email');
+        $this->assertEquals($conversationContext->getAttributeValue('current_conversation'), 'with_incoming_intent_conditions');
+        $this->assertEquals($conversationContext->getAttributeValue('current_scene'), 'get_scene');
+        $this->assertEquals($conversationContext->getAttributeValue('next_intent'), 'response');
+    }
+
     public function getTestConversation()
     {
         return <<<EOT
@@ -119,5 +169,70 @@ conversation:
             i: doing_dandy
             completes: true
 EOT;
+    }
+
+    public function testConversationWithOutgoingIntentConditions()
+    {
+        return <<<EOT
+conversation:
+  id: with_outgoing_intent_conditions
+  scenes:
+    opening_scene:
+      intents:
+        - u: 
+            i: hello_bot
+        - b: 
+            i: response_with_name
+            conditions:
+                operation: is_set
+                attributes:
+                    attribute1: user.first_name
+        - b:
+            i: response_without_name
+            conditions:
+                operation: is_not_set
+                attributes:
+                    attribute1: user.first_name
+            completes: true
+EOT;
+
+    }
+
+    public function testConversationWithIncomingIntentConditions()
+    {
+        return <<<EOT
+conversation:
+  id: with_incoming_intent_conditions
+  scenes:
+      opening_scene:
+        intents:
+            - u:
+                i: ask_to_send_me_email
+                conditions:
+                    operation: is_set
+                    attributes:
+                        attribute1: user.email
+                scene: send_scene
+            - u:
+                i: ask_to_send_me_email
+                conditions:
+                    operation: is_not_set
+                    attributes:
+                        attribute1: user.email
+                scene: get_scene
+      get_scene:
+        intents:
+            - b:
+                i: ask_for_email
+            - u:
+                i: send_email
+                scene: send_scene
+      send_scene:
+        intents:
+            - b:
+                i: response
+                completes: true
+EOT;
+
     }
 }
