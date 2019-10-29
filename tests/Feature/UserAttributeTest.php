bcef49d1c88ef1a15801179f98dbfa469ae30316
Stuart Haigh <stuarth@greenshootlabs.com>
2019-10-29 17:46:45 +0000

    failing test when trying to get user attribute for a new user

---
+++ tests/Feature/UserAttributeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OpenDialogAi\Core\Tests\Feature;
+
+use OpenDialogAi\ContextEngine\Facades\ContextService;
+use OpenDialogAi\Core\Controllers\OpenDialogController;
+use OpenDialogAi\Core\Tests\TestCase;
+use OpenDialogAi\Core\Tests\Utils\UtteranceGenerator;
+
+class UserAttributeTest extends TestCase
+{
+    public function testUserAttributes()
+    {
+        $utterance = UtteranceGenerator::generateChatOpenUtterance('WELCOME');
+        $this->activateConversation($this->noMatchConversation());
+
+        resolve(OpenDialogController::class)->runConversation($utterance);
+
+        $this->assertTrue(ContextService::getUserContext()->getUser()->hasAttribute('first_name'));
+    }
+}
