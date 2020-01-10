ad409c60c60dc1ac6c70a363aab0ea9abe27c026
Nicolò Caruso <nicolo@istos.it>
2020-01-10 10:55:28 +0100

    Improve tests for Import and Export a Conversation

--- src/Console/Commands/tests/ImportExportConversationTest.php
+++ src/Console/Commands/tests/ImportExportConversationTest.php
@@ -3,6 +3,8 @@
 namespace OpenDialogAi\Core\Console\Commands\tests;
 
 use Illuminate\Support\Facades\Artisan;
+use OpenDialogAi\ConversationBuilder\Conversation;
+use OpenDialogAi\ConversationEngine\ConversationStore\ConversationStoreInterface;
 use OpenDialogAi\Core\Tests\TestCase;
 
 class ImportExportConversationTest extends TestCase
@@ -18,6 +20,15 @@ class ImportExportConversationTest extends TestCase
     {
         $exitCode = Artisan::call('conversation:import ' . dirname(__FILE__) . '/example_conversation --activate --yes');
         $this->assertEquals(0, $exitCode);
+
+        $conversation = Conversation::where(['name' => 'example_conversation'])->first();
+        $this->assertEquals(1, $conversation->id);
+
+        $conversationStore = $this->app->make(ConversationStoreInterface::class);
+        $conversation = $conversationStore->getConversationTemplateByUid($conversation->graph_uid);
+        $scene = $conversation->getOpeningScenes()->first()->value;
+        $intent = $scene->getIntentsSaidByBotInOrder()->first()->value;
+        $this->assertEquals('intent.core.NoMatchResponse', $intent->getLabel());
     }
 
     public function testExportConversation()
@@ -27,5 +38,11 @@ class ImportExportConversationTest extends TestCase
         $this->expectOutputRegex('/^.*id: example_conversation.*$/');
         $exitCode = Artisan::call('conversation:export example_conversation');
         $this->assertEquals(0, $exitCode);
+
+        $exitCode = Artisan::call('conversation:export example_conversation -f exported_conversation');
+        $this->assertEquals(0, $exitCode);
+
+        $this->assertFileExists('exported_conversation');
+        unlink('exported_conversation');
     }
 }
