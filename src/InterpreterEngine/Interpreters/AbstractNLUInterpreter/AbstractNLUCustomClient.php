951523ce9a905dcb2decdc32dbc22da6a85bea58
elliotmassen <elliotm@greenshootlabs.com>
2020-02-27 11:31:15 +0000

    Allowing for custom NLU clients in AbstractNLU classes

---
+++ src/InterpreterEngine/Interpreters/AbstractNLUInterpreter/AbstractNLUCustomClient.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace OpenDialogAi\InterpreterEngine\Interpreters\AbstractNLUInterpreter;
+
+
+use Exception;
+use Illuminate\Support\Facades\Log;
+
+abstract class AbstractNLUCustomClient extends AbstractNLUClient
+{
+    /**
+     * @param $message
+     * @return AbstractNLUCustomRequest
+     */
+    abstract public function sendRequest($message): AbstractNLUCustomRequest;
+
+    /**
+     * Sends a message to the NLU service and creates a response object
+     * @param $message
+     * @return AbstractNLUResponse
+     * @throws AbstractNLURequestFailedException
+     */
+    public function query($message): AbstractNLUResponse
+    {
+        try {
+            $request = $this->sendRequest($message);
+        } catch (Exception $e) {
+            throw new AbstractNLURequestFailedException($e->getMessage());
+        }
+
+        $responseData = $request->getContents();
+        if ($request->isSuccessful()) {
+            Log::debug("Successful client call", ['response' => $responseData]);
+            return $this->createResponse($responseData);
+        } else {
+            Log::warning("Unsuccessful client call", ['response' => $responseData]);
+            throw new AbstractNLURequestFailedException("Client call failed with a non 200 response, please check the logs");
+        }
+    }
+}
---
+++ src/InterpreterEngine/Interpreters/AbstractNLUInterpreter/AbstractNLUCustomRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace OpenDialogAi\InterpreterEngine\Interpreters\AbstractNLUInterpreter;
+
+
+abstract class AbstractNLUCustomRequest
+{
+    /**
+     * This method gets the returned contents of the request.
+     *
+     * @return mixed
+     */
+    abstract public function getContents();
+
+    /**
+     * This method returns a boolean representing whether or not the request was successful or not
+     *
+     * @return bool
+     */
+    abstract public function isSuccessful(): bool;
+}
