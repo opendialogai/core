fe697d80c53b6310bf6b45ed58c0173477abcd5d
Stuart Haigh <stuarth@greenshootlabs.com>
2020-09-11 14:25:46 +0100

    allows callback values with escaped dots (`\.`)

--- tests/Unit/Attribute/CallbackValueParserTest.php
+++ tests/Unit/Attribute/CallbackValueParserTest.php
@@ -25,4 +25,17 @@ class CallbackValueParserTest extends TestCase
         $this->assertEquals("name.value.broken", $parseButtonValue['attribute_value']);
         $this->assertEquals(CallbackValueParser::CALLBACK_VALUE, $parseButtonValue['attribute_name']);
     }
+
+    public function testCallbackValueEscaped()
+    {
+        $buttonValue = "name.value\.escaped";
+        $parseButtonValue = CallbackValueParser::parseCallbackValue($buttonValue);
+        $this->assertEquals("value.escaped", $parseButtonValue['attribute_value']);
+        $this->assertEquals('name', $parseButtonValue['attribute_name']);
+
+        $buttonValue = "name.value\.e\.s\.c\.a\.p\.e\.d";
+        $parseButtonValue = CallbackValueParser::parseCallbackValue($buttonValue);
+        $this->assertEquals("value.e.s.c.a.p.e.d", $parseButtonValue['attribute_value']);
+        $this->assertEquals('name', $parseButtonValue['attribute_name']);
+    }
 }
