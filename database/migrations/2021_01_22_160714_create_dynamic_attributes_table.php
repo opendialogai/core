b5db1e7e9591ed82ccde7e354cb7890b96de1bd5
Benjamin Dudiak-Fry <bend@deeson.co.uk>
2021-01-25 16:00:51 +0000

    Added DynamicAttribute model and migration. Models attributes stored in the database.

---
+++ database/migrations/2021_01_22_160714_create_dynamic_attributes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDynamicAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dynamic_attributes', function (Blueprint $table) {
+            $table->string('id')->unique();
+            $table->string('type');
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dynamic_attributes');
+    }
+}
---
+++ src/DynamicAttribute.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace OpenDialogAi\Core;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DynamicAttribute extends Model
+{
+    static private $validIdPattern = "/^([a-z]+_)*[a-z]+$/";
+    static private $validTypePattern = "/^attribute\.[A-Za-z]*\.[A-Za-z_]*$/";
+
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+    protected $table = 'dynamic_attributes';
+    protected $fillable = ['id', 'type'];
+    protected $keyType = 'string';
+    protected $primaryKey = 'id';
+
+
+    /**
+     * Checks if the id of the DynamicAttribute is in the right format
+     *
+     * @param string $id
+     * @return bool
+     */
+    public static function isValidId(string $id) : bool
+    {
+        return preg_match(static::$validIdPattern, $id) === 1;
+    }
+
+    /**
+     * Checks if the type of the DynamicAttribute is in the right format
+     *
+     * @param string $type
+     * @return bool
+     */
+    public static function isValidType(string $type) : bool
+    {
+        return preg_match(static::$validTypePattern, $type) === 1;
+    }
+
+}
