071a10f489cea4ddbdc6ee522d72f711cf169e68
elliotmassen <elliotm@greenshootlabs.com>
2021-02-03 16:45:43 +0000

    Adding OpenDialogComponent classes

---
+++ src/Components/OpenDialogComponentInterface.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace OpenDialogAi\Core\Components;
+
+
+interface OpenDialogComponentInterface
+{
+    /**
+     * Returns an object containing relevant information about the component.
+     *
+     * @return OpenDialogComponentData
+     * @throws MissingRequiredComponentDataException
+     * @throws InvalidComponentDataException
+     */
+    public static function getComponentData(): OpenDialogComponentData;
+
+    /**
+     * The human-readable name of the OpenDialog component.
+     *
+     * @return string|null
+     */
+    public static function getComponentName(): ?string;
+
+    /**
+     * The human-readable description of the OpenDialog component.
+     */
+    public static function getComponentDescription(): ?string;
+
+    /**
+     * The type of the OpenDialog component (eg. 'action', 'interpreter', etc).
+     *
+     * @throws MissingRequiredComponentDataException
+     * @throws InvalidComponentDataException
+     */
+    public static function getComponentType(): string;
+
+    /**
+     * The source of the OpenDialog component which records where the component was registered (eg. 'core', 'app', 'custom').
+     * For now this is a static method meaning the source is predetermined prior to run-time.
+     * TODO: In the future this could be an instance method but it would require all components to be instantiated at
+     *  registration which does not currently consistently happen across all component types.
+     *
+     * @throws MissingRequiredComponentDataException
+     * @throws InvalidComponentDataException
+     */
+    public static function getComponentSource(): string;
+
+    /**
+     * The ID of the OpenDialog component. Depending on the component type this may be a namespaced component ID (such as
+     * 'interpreter.core.callback' interpreter) or a regular ID (such as 'eq' operation)
+     *
+     * @throws MissingRequiredComponentDataException
+     */
+    public static function getComponentId(): string;
+}
