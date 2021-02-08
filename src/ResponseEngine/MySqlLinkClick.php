000b53dce034a533752a2bbea5716bf8360bf93f
elliotmassen <elliotm@greenshootlabs.com>
2021-02-08 15:34:52 +0000

    Ensuring MysqlLinkClick implements its interface

--- src/ResponseEngine/MySqlLinkClick.php
+++ src/ResponseEngine/MySqlLinkClick.php
@@ -3,12 +3,14 @@
 namespace OpenDialogAi\ResponseEngine;
 
 use DateTime;
+use OpenDialogAi\AttributeEngine\CoreAttributes\UtteranceAttribute;
 use OpenDialogAi\Core\Utterances\Webchat\WebchatUrlClickUtterance;
 
 class MySqlLinkClick implements LinkClickInterface
 {
-    public function save(WebchatUrlClickUtterance $utterance): LinkClick
+    public function save(UtteranceAttribute $utterance): LinkClick
     {
+        /** @var WebchatUrlClickUtterance $utterance */
         $timestamp = DateTime::createFromFormat('U.u', $utterance->getTimestamp())->format('Y-m-d H:i:s');
 
         $linkClick = new LinkClick([
