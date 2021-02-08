33f40a0598767fcde96fcd8b9a5d58bd2be71e5b
elliotmassen <elliotm@greenshootlabs.com>
2021-02-08 18:35:17 +0000

    Adding properties to DynamicAttribute model phpdoc

--- src/AttributeEngine/DynamicAttribute.php
+++ src/AttributeEngine/DynamicAttribute.php
@@ -4,9 +4,14 @@ namespace OpenDialogAi\AttributeEngine;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $attribute_id
+ * @property string $attribute_type
+ */
 class DynamicAttribute extends Model
 {
     protected $table = 'dynamic_attributes';
-    protected $fillable = ['attribute_id', 'attribute_type'];
 
+    protected $fillable = ['attribute_id', 'attribute_type'];
 }
