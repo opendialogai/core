f5109d1def426464399da92f32c2d4193407baf0
Ronald Ashri <ronald@istos.it>
2021-02-15 18:49:44 +0100

    Starting intent selector.

--- src/ConversationEngine/Reasoners/StartingIntentSelector.php
+++ src/ConversationEngine/Reasoners/StartingIntentSelector.php
@@ -3,8 +3,13 @@
 
 namespace OpenDialogAi\ConversationEngine\Reasoners;
 
+use OpenDialogAi\AttributeEngine\CoreAttributes\UtteranceAttribute;
+use OpenDialogAi\ContextEngine\Contexts\User\UserContext;
+use OpenDialogAi\ContextEngine\Facades\ContextService;
 use OpenDialogAi\Core\Conversation\Facades\ConversationDataClient;
+use OpenDialogAi\Core\Conversation\Intent;
 use OpenDialogAi\Core\Conversation\IntentCollection;
+use OpenDialogAi\InterpreterEngine\Facades\InterpreterService;
 
 /**
  * The TurnSelector should evaluate conditions against turns to select
@@ -14,16 +19,34 @@ class StartingIntentSelector
 {
     public static function selectStartingIntents($turns): IntentCollection
     {
+        // These are all the possible intents that could start a conversation
         $intents = ConversationDataClient::getAllStartingIntents($turns);
 
+        // We first reduce the set to just those that have passing conditions
         $conditionPassingIntents = $intents->filter(function ($intent) {
             ConditionFilter::checkConditions($intent);
         });
 
-        $interpreterMatchingIntents = $intents->filter(function ($intent) {
-            IntentFilter::matchIntent($intent);
+        // Now we can pass each intent through interpreters and interpret given the utterance
+        $utterance = ContextService::getAttribute(UtteranceAttribute::UTTERANCE, UserContext::USER_CONTEXT);
+        $interpretedIntents = $conditionPassingIntents->map(function (Intent $intent) use ($utterance) {
+            // Get the interpreter defined in the current model
+            $interpreter = $intent->getInterpreter();
+            $interpretedIntents = new IntentCollection();
+
+            if ($interpreter) {
+                $interpretedIntents = InterpreterService::interpret($intent->getInterpreter(), $utterance);
+            } else {
+                $interpretedIntents = InterpreterService::interpretDefaultInterpreter($utterance);
+            }
+            $intent->addInterpretedIntents($interpretedIntents);
         });
 
-        return $interpreterMatchingIntents;
+        // With the interpretations in place let us do a match
+        $matchingIntents = $interpretedIntents->filter(function (Intent $intent){
+            return $intent->checkForMatch();
+        });
+
+        return $matchingIntents;
     }
 }
