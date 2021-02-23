27e41749884c07908aed786d488f5c0da1906fc4
elliotmassen <elliotm@greenshootlabs.com>
2021-02-23 15:41:56 +0000

    Implementing IntentRanker

--- src/ConversationEngine/Tests/IntentRankerTest.php
+++ src/ConversationEngine/Tests/IntentRankerTest.php
@@ -3,6 +3,8 @@
 namespace OpenDialogAi\ConversationEngine\Tests;
 
 
+use OpenDialogAi\AttributeEngine\AttributeBag\BasicAttributeBag;
+use OpenDialogAi\AttributeEngine\Contracts\AttributeBag;
 use OpenDialogAi\AttributeEngine\Facades\AttributeResolver;
 use OpenDialogAi\ConversationEngine\Exceptions\EmptyCollectionException;
 use OpenDialogAi\ConversationEngine\Reasoners\IntentRanker;
@@ -26,7 +28,7 @@ class IntentRankerTest extends TestCase
      */
     public function testWithSingleIntent()
     {
-        $intent = Intent::createIntent('test_intent', 1);
+        $intent = $this->createIntentWithInterpretation('test_intent', 1);
         $intents = new IntentCollection([$intent]);
 
         $topRankingIntent = IntentRanker::getTopRankingIntent($intents);
@@ -39,20 +41,20 @@ class IntentRankerTest extends TestCase
      */
     public function testWithMultipleIntentsNoAttributes()
     {
-        $expectedIntent = Intent::createIntent('test_intent2', 0.75);
+        $expectedIntent = $this->createIntentWithInterpretation('test_intent2', 0.75);
 
         $intentArray = [];
-        $intentArray[] = Intent::createIntent('test_intent', 0);
+        $intentArray[] = $this->createIntentWithInterpretation('test_intent', 0);
         $intentArray[] = $expectedIntent;
-        $intentArray[] = Intent::createIntent('test_intent3', 0.75);
-        $intentArray[] = Intent::createIntent('test_intent4', 0.25);
-        $intentArray[] = Intent::createIntent('test_intent5', 0.5);
-        $intents = new IntentCollection([$intentArray]);
+        $intentArray[] = $this->createIntentWithInterpretation('test_intent3', 0.75);
+        $intentArray[] = $this->createIntentWithInterpretation('test_intent4', 0.25);
+        $intentArray[] = $this->createIntentWithInterpretation('test_intent5', 0.5);
+        $intents = new IntentCollection($intentArray);
 
         $topRankingIntent = IntentRanker::getTopRankingIntent($intents);
 
         // Two intents shared the highest confidence (0.75), we should take the one that appears earliest in the collection
-        $this->assertEquals($expectedIntent, $topRankingIntent);
+        $this->assertSame($expectedIntent, $topRankingIntent);
     }
 
     /**
@@ -60,24 +62,67 @@ class IntentRankerTest extends TestCase
      */
     public function testWithMultipleIntentsWithAttributes()
     {
-        $expectedIntent = Intent::createIntent('test_intent3', 0.75);
-        $expectedIntent->addAttribute(AttributeResolver::getAttributeFor('first_name', 'test'));
-        $expectedIntent->addAttribute(AttributeResolver::getAttributeFor('last_name', 'test'));
+        $expectedAttributeBag = new BasicAttributeBag();
+        $expectedAttributeBag->addAttribute(AttributeResolver::getAttributeFor('first_name', 'test'));
+        $expectedAttributeBag->addAttribute(AttributeResolver::getAttributeFor('last_name', 'test'));
+        $expectedIntent = $this->createIntentWithInterpretationWithAttributes(
+            'test_intent3',
+            0.75,
+            $expectedAttributeBag
+        );
 
-        $notExpectedIntent = Intent::createIntent('test_intent2', 0.75);
-        $notExpectedIntent->addAttribute(AttributeResolver::getAttributeFor('first_name', 'test'));
+        $notExpectedAttributeBag = new BasicAttributeBag();
+        $notExpectedAttributeBag->addAttribute(AttributeResolver::getAttributeFor('first_name', 'test'));
+        $notExpectedIntent = $this->createIntentWithInterpretationWithAttributes(
+            'test_intent2',
+            0.75,
+            $notExpectedAttributeBag
+        );
 
         $intentArray = [];
-        $intentArray[] = Intent::createIntent('test_intent', 0);
+        $intentArray[] = $this->createIntentWithInterpretation('test_intent', 0);
         $intentArray[] = $notExpectedIntent;
         $intentArray[] = $expectedIntent;
-        $intentArray[] = Intent::createIntent('test_intent4', 0.25);
-        $intentArray[] = Intent::createIntent('test_intent5', 0.5);
-        $intents = new IntentCollection([$intentArray]);
+        $intentArray[] = $this->createIntentWithInterpretation('test_intent4', 0.25);
+        $intentArray[] = $this->createIntentWithInterpretation('test_intent5', 0.5);
+        $intents = new IntentCollection($intentArray);
 
         $topRankingIntent = IntentRanker::getTopRankingIntent($intents);
 
         // Two intents shared the highest confidence (0.75), but one had more (matching expected) attributes
-        $this->assertEquals($expectedIntent, $topRankingIntent);
+        $this->assertSame($expectedIntent, $topRankingIntent);
+    }
+
+    /**
+     * @param string $id
+     * @param float $confidence
+     * @return Intent
+     */
+    private function createIntentWithInterpretation(string $id, float $confidence): Intent
+    {
+        $intent = Intent::createIntent($id, $confidence);
+
+        $intent->addInterpretedIntents(new IntentCollection([clone $intent]));
+        $intent->checkForMatch();
+
+        return $intent;
+    }
+
+    /**
+     * @param string $id
+     * @param float $confidence
+     * @param AttributeBag $attributeBag
+     * @return Intent
+     */
+    private function createIntentWithInterpretationWithAttributes(string $id, float $confidence, AttributeBag $attributeBag): Intent
+    {
+        $intent = $this->createIntentWithInterpretation($id, $confidence);
+        $interpretedIntent = $intent->getInterpretation();
+
+        foreach ($attributeBag->getAttributes() as $attribute) {
+            $interpretedIntent->addAttribute($attribute);
+        }
+
+        return $intent;
     }
 }
