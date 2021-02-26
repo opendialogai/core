50d6ae8b7ef953f878c20c0c4ec8853526de4000
elliotmassen <elliotm@greenshootlabs.com>
2021-02-26 16:53:40 +0000

    Renaming ConversationEngineTest to UtteranceReasonerTest

--- src/ConversationEngine/Tests/UtteranceReasonerTest.php
+++ src/ConversationEngine/Tests/UtteranceReasonerTest.php
@@ -2,34 +2,17 @@
 
 namespace OpenDialogAi\ConversationEngine\Tests;
 
+use Mockery\MockInterface;
 use OpenDialogAi\AttributeEngine\CoreAttributes\UserAttribute;
-use OpenDialogAi\AttributeEngine\CoreAttributes\UtteranceAttribute;
 use OpenDialogAi\ContextEngine\Contexts\User\UserDataClient;
-use OpenDialogAi\ConversationEngine\ConversationEngine;
-use OpenDialogAi\ConversationEngine\ConversationEngineInterface;
 use OpenDialogAi\ConversationEngine\Exceptions\IncomingUtteranceNotValid;
 use OpenDialogAi\ConversationEngine\Reasoners\UtteranceReasoner;
 use OpenDialogAi\Core\Tests\TestCase;
 use OpenDialogAi\Core\Tests\Utils\UserGenerator;
 use OpenDialogAi\Core\Tests\Utils\UtteranceGenerator;
 
-class ConversationEngineTest extends TestCase
+class UtteranceReasonerTest extends TestCase
 {
-    /* @var ConversationEngine */
-    private $conversationEngine;
-
-    /* @var UtteranceAttribute */
-    private $utterance;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->conversationEngine = resolve(ConversationEngineInterface::class);
-
-        $this->utterance = UtteranceGenerator::generateChatOpenUtterance('hello_bot');
-    }
-
     public function testUtteranceAnalysisWhenMissingUserId()
     {
         $utterance = UtteranceGenerator::generateUtteranceWithoutUserId();
@@ -41,16 +24,18 @@ class ConversationEngineTest extends TestCase
     {
         $utterance = UtteranceGenerator::generateChatOpenUtterance(
             'intent.core.welcome',
-            UserGenerator::generateUtteranceUserWithCustomAttributes());
+            UserGenerator::generateUtteranceUserWithCustomAttributes()
+        );
 
-        $this->mock(UserDataClient::class, function ($mock) {
-            $mock->shouldReceive('loadAttribute')->once()
+        $this->mock(UserDataClient::class, function (MockInterface $mock) {
+            $mock->shouldReceive('loadAttribute')
+                ->once()
                 ->andReturn(UserGenerator::generateCurrentUserWithCustomAttributes()
             );
         });
 
         $userAttribute = UtteranceReasoner::analyseUtterance($utterance);
-        $this->assertEquals(UserAttribute::CURRENT_USER, $userAttribute->getId());
 
+        $this->assertEquals(UserAttribute::CURRENT_USER, $userAttribute->getId());
     }
 }
