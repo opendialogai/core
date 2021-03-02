d2c23b7345fd1d3d35cae0e59426702c9d660bc1
Benjamin Dudiak-Fry <bend@deeson.co.uk>
2021-03-02 10:05:40 +0000

    Formatting.

--- src/GraphQLClient/GraphQLClientServiceProvider.php
+++ src/GraphQLClient/GraphQLClientServiceProvider.php
@@ -22,15 +22,17 @@ class GraphQLClientServiceProvider extends ServiceProvider
             $baseUrl = config('opendialog.graphql.DGRAPH_BASE_URL');
             $port = config('opendialog.graphql.DGRAPH_PORT');
             if (!$baseUrl) {
-                throw new GraphQLClientConfigException("opendialog.graphql.DGRAPH_BASE_URL config value is missing. You must set a value, e.g https://example.com/");
+                throw new GraphQLClientConfigException("opendialog.graphql.DGRAPH_BASE_URL config value is missing.");
             }
             if (!$port) {
-                throw new GraphQLClientConfigException("opendialog.graphql.DGRAPH_PORT config value is missing. You must set a a port number, e.g 443");
+                throw new GraphQLClientConfigException("opendialog.graphql.DGRAPH_PORT config value is missing.");
             }
 
             $instanceType = config('opendialog.graphql.DGRAPH_INSTANCE_TYPE');
             if (!$instanceType) {
-                throw new GraphQLClientConfigException("opendialog.graphql.DGRAPH_INSTANCE_TYPE config value is missing. If you are connecting to your own DGraph instance, set it to 'DGRAPH'. If you are connecting to a SlashGraphQL instance, use 'SLASH_GRAPHQL'.");
+                throw new GraphQLClientConfigException("opendialog.graphql.DGRAPH_INSTANCE_TYPE config value is missing.
+                 If you are connecting to your own DGraph instance, set it to 'DGRAPH'.
+                 If you are connecting to a SlashGraphQL instance, use 'SLASH_GRAPHQL'.");
             }
 
             $headers = [];
@@ -38,14 +40,16 @@ class GraphQLClientServiceProvider extends ServiceProvider
                 if ($authToken = config('opendialog.graphql.DGRAPH_AUTH_TOKEN')) {
                     $headers['X-Dgraph-AuthToken'] = $authToken;
                 } else {
-                    throw new GraphQLClientConfigException("opendialog.graphql.DGRAPH_AUTH_TOKEN config value is missing. You must set this value when using 'DGRAPH' for the opendialog.graphql.DGRAPH_INSTANCE_TYPE");
+                    throw new GraphQLClientConfigException("opendialog.graphql.DGRAPH_AUTH_TOKEN config value is missing.
+                     You must set this value when using 'DGRAPH' for the opendialog.graphql.DGRAPH_INSTANCE_TYPE");
                 }
             }
             if ($instanceType === 'SLASH_GRAPHQL') {
                 if ($apiKey = config('opendialog.graphql.SLASH_GRAPHQL_API_KEY')) {
                     $headers['X-Auth-Token'] = $apiKey;
                 } else {
-                    throw new GraphQLClientConfigException("opendialog.graphql.SLASH_GRAPHQL_API_KEY config value is missing. You must set this value when using 'SLASH_GRAPHQL' for the opendialog.graphql.DGRAPH_INSTANCE_TYPE");
+                    throw new GraphQLClientConfigException("opendialog.graphql.SLASH_GRAPHQL_API_KEY config value is missing.
+                     You must set this value when using 'SLASH_GRAPHQL' for the opendialog.graphql.DGRAPH_INSTANCE_TYPE");
                 }
             }
 
