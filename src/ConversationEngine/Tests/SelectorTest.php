91c2716477eb030ca47e5bd06a3443792559b636
elliotmassen <elliotm@greenshootlabs.com>
2021-03-02 10:58:30 +0000

    Adding tests for intent context

--- src/ConversationEngine/Tests/SelectorTest.php
+++ src/ConversationEngine/Tests/SelectorTest.php
@@ -2,6 +2,7 @@
 
 namespace OpenDialogAi\ConversationEngine\Tests;
 
+use OpenDialogAi\AttributeEngine\Facades\AttributeResolver;
 use OpenDialogAi\ContextEngine\Contexts\BaseContexts\SessionContext;
 use OpenDialogAi\ContextEngine\Facades\ContextService;
 use OpenDialogAi\ConversationEngine\Exceptions\EmptyCollectionException;
@@ -89,10 +90,70 @@ class SelectorTest extends TestCase
             );
 
         /** @var Scenario[]|ScenarioCollection $selectedIntents */
-        $selectedIntents = IntentSelector::selectRequestIntents(new TurnCollection([$turn]));
+        $selectedIntents = IntentSelector::selectRequestIntents(new TurnCollection([$turn]), true);
 
         // There were three objects, but one was filtered by interpretation, the other by conditions
         $this->assertCount(1, $selectedIntents);
+        $this->assertContains($intent3, $selectedIntents);
+    }
+
+    /**
+     * @throws EmptyCollectionException
+     */
+    public function testIntentSelectorIntentContextConditions()
+    {
+        $passingIntentContextConditions = new ConditionCollection([
+            new Condition('eq', ['attribute' => '_intent.first_name'], ['value' => 'test'])
+        ]);
+
+        $failingIntentContextConditions = new ConditionCollection([
+            new Condition('eq', ['attribute' => '_intent.first_name'], ['value' => 'unknown'])
+        ]);
+
+        $intent1 = ConversationGenerator::createUserIntent('intent_1', $failingIntentContextConditions);
+        $intent1->setConfidence(1);
+        $intent1Interpreted = clone $intent1;
+        $intent1Interpreted->addAttribute(AttributeResolver::getAttributeFor('first_name', 'test'));
+        $intent1->addInterpretedIntents(new IntentCollection([$intent1Interpreted]));
+        $intent1->checkForMatch();
+
+        $intent2 = ConversationGenerator::createUserIntent('intent_2', $passingIntentContextConditions);
+        $intent2->setConfidence(1);
+        $intent2Interpreted = clone $intent2;
+        $intent2Interpreted->addAttribute(AttributeResolver::getAttributeFor('first_name', 'test'));
+        $intent2->addInterpretedIntents(new IntentCollection([$intent2Interpreted]));
+        $intent2->checkForMatch();
+
+        $intent3 = ConversationGenerator::createUserIntent('intent_3', $passingIntentContextConditions);
+        $intent3->setConfidence(1);
+        $intent3Interpreted = clone $intent3;
+        $intent3Interpreted->addAttribute(AttributeResolver::getAttributeFor('first_name', 'test'));
+        $intent3->addInterpretedIntents(new IntentCollection([$intent3Interpreted]));
+        $intent3->checkForMatch();
+
+        $intents = new IntentCollection([$intent1, $intent2, $intent3]);
+
+        $turn = ConversationGenerator::createTurn('turn_1');
+        $turn->setRequestIntents($intents);
+
+        ConversationDataClient::shouldReceive('getAllRequestIntents')
+            ->once()
+            ->andReturn($intents);
+
+        InterpreterService::shouldReceive('interpretDefaultInterpreter')
+            ->times(3)
+            ->andReturn(
+                new IntentCollection([$intent1]),
+                new IntentCollection([$intent2]),
+                new IntentCollection([$intent3])
+            );
+
+        /** @var Scenario[]|ScenarioCollection $selectedIntents */
+        $selectedIntents = IntentSelector::selectRequestIntents(new TurnCollection([$turn]), true);
+
+        // There were three objects, none were filtered by interpretation, one failing conditions
+        $this->assertCount(2, $selectedIntents);
+        $this->assertContains($intent2, $selectedIntents);
         $this->assertContains($intent3, $selectedIntents);
     }
 
@@ -129,7 +190,7 @@ class SelectorTest extends TestCase
             );
 
         /** @var Scenario[]|ScenarioCollection $selectedIntents */
-        $selectedIntents = IntentSelector::selectResponseIntents(new TurnCollection([$turn]));
+        $selectedIntents = IntentSelector::selectResponseIntents(new TurnCollection([$turn]), true);
 
         // There were three objects, but one was filtered by interpretation, the other by conditions
         $this->assertCount(1, $selectedIntents);
@@ -169,7 +230,7 @@ class SelectorTest extends TestCase
             );
 
         /** @var Scenario[]|ScenarioCollection $selectedIntents */
-        $selectedIntents = IntentSelector::selectRequestIntentsById(new TurnCollection([$turn]), 'intent_1');
+        $selectedIntents = IntentSelector::selectRequestIntentsById(new TurnCollection([$turn]), 'intent_1', true);
 
         // There were three objects with the desired ID, but one wasn't interpreted and the other was filtered out by conditions
         $this->assertCount(1, $selectedIntents);
@@ -209,7 +270,7 @@ class SelectorTest extends TestCase
             );
 
         /** @var Scenario[]|ScenarioCollection $selectedIntents */
-        $selectedIntents = IntentSelector::selectResponseIntentsById(new TurnCollection([$turn]), 'intent_1');
+        $selectedIntents = IntentSelector::selectResponseIntentsById(new TurnCollection([$turn]), 'intent_1', true);
 
         // There were three objects with the desired ID, but one wasn't interpreted and the other was filtered out by conditions
         $this->assertCount(1, $selectedIntents);
