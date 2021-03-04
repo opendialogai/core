b01322b55c65b2d9dd114596191c705b7e834693
Benjamin Dudiak-Fry <bend@deeson.co.uk>
2021-03-04 08:34:12 +0000

    Uncommented SlashGraphQLEndpointTest.php tests. Now relying on the skip group to prevent them being run. Factored out graphql schema into separate gql file.

--- src/GraphQLClient/Tests/SlashGraphQLEndpointTest.php
+++ src/GraphQLClient/Tests/SlashGraphQLEndpointTest.php
@@ -26,36 +26,36 @@ class SlashGraphQLEndpointTest extends TestCase
         ];
     }
 
-//    /**
-//     * @group skip
-//     */
-//    public function testGetSchemaFromAdminEndpointWithInvalidAuth() {
-//        //TODO: Reintroduce test. Need a custom slashGraphQL instance for testing.
-//        $this->setConfigValue('opendialog.graphql.DGRAPH_INSTANCE_TYPE', 'SLASH_GRAPHQL');
-//        $this->setConfigValue('opendialog.graphql.DGRAPH_BASE_URL', '<removed>');
-//        $this->setConfigValue('opendialog.graphql.DGRAPH_PORT', 443);
-//        $this->setConfigValue('opendialog.graphql.DGRAPH_AUTH_TOKEN', null);
-//        $this->setConfigValue('opendialog.graphql.SLASH_GRAPHQL_API_KEY', 'invalid');
-//        $client = resolve(GraphQLClientInterface::class);
-//
-//        $this->expectException(ClientException::class);
-//        $client->request("/admin", $this->adminSchemaQuery());
-//    }
-//
-//    /**
-//     * @group skip
-//     */
-//    public function testGetSchemaFromAdminEndpoint() {
-//        //TODO: Reintroduce test. Need a custom slashGraphQL instance for testing.
-//        $this->setConfigValue('opendialog.graphql.DGRAPH_INSTANCE_TYPE', 'SLASH_GRAPHQL');
-//        $this->setConfigValue('opendialog.graphql.DGRAPH_BASE_URL', '<removed>');
-//        $this->setConfigValue('opendialog.graphql.DGRAPH_PORT', 443);
-//        $this->setConfigValue('opendialog.graphql.DGRAPH_AUTH_TOKEN', null);
-//        $this->setConfigValue('opendialog.graphql.SLASH_GRAPHQL_API_KEY', '<removed>');
-//        $client = resolve(GraphQLClientInterface::class);
-//
-//        $response = $client->request("/admin", $this->adminSchemaQuery());
-//        $this->assertIsArray($response);
-//        $this->assertIsString($response['data']['getGQLSchema']['schema']);
-//    }
+    /**
+     * @group skip
+     */
+    public function testGetSchemaFromAdminEndpointWithInvalidAuth() {
+        //TODO: Reintroduce test. Need a custom slashGraphQL instance for testing.
+        $this->setConfigValue('opendialog.graphql.DGRAPH_INSTANCE_TYPE', 'SLASH_GRAPHQL');
+        $this->setConfigValue('opendialog.graphql.DGRAPH_BASE_URL', '<removed>');
+        $this->setConfigValue('opendialog.graphql.DGRAPH_PORT', 443);
+        $this->setConfigValue('opendialog.graphql.DGRAPH_AUTH_TOKEN', null);
+        $this->setConfigValue('opendialog.graphql.SLASH_GRAPHQL_API_KEY', 'invalid');
+        $client = resolve(GraphQLClientInterface::class);
+
+        $this->expectException(ClientException::class);
+        $client->request("/admin", $this->adminSchemaQuery());
+    }
+
+    /**
+     * @group skip
+     */
+    public function testGetSchemaFromAdminEndpoint() {
+        //TODO: Reintroduce test. Need a custom slashGraphQL instance for testing.
+        $this->setConfigValue('opendialog.graphql.DGRAPH_INSTANCE_TYPE', 'SLASH_GRAPHQL');
+        $this->setConfigValue('opendialog.graphql.DGRAPH_BASE_URL', '<removed>');
+        $this->setConfigValue('opendialog.graphql.DGRAPH_PORT', 443);
+        $this->setConfigValue('opendialog.graphql.DGRAPH_AUTH_TOKEN', null);
+        $this->setConfigValue('opendialog.graphql.SLASH_GRAPHQL_API_KEY', '<removed>');
+        $client = resolve(GraphQLClientInterface::class);
+
+        $response = $client->request("/admin", $this->adminSchemaQuery());
+        $this->assertIsArray($response);
+        $this->assertIsString($response['data']['getGQLSchema']['schema']);
+    }
 }
